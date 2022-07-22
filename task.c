ac7c7cc736d866af63514c8893555eb769c43459
AadityaShah2716 <109588973+AadityaShah2716@users.noreply.github.com>
2022-07-22 14:16:40 +0530

    Create task.c

---
+++ task.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int row,col,n=1;
+
+
+
+	for(row =1;row<=10;row++)
+	{
+		for(col=1;col<=row;col++)
+		{
+		if(row%2!=0)
+		{
+			printf("%d",row,n++);
+		}
+
+
+}
+
+ printf("\n");
+}
+return 0;
+ }
