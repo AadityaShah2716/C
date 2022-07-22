05d8643c6b17d7ef0fe62298ccdbef5578ab4f64
AadityaShah2716 <109588973+AadityaShah2716@users.noreply.github.com>
2022-07-22 14:06:33 +0530

    Initial commit

---
+++ assignment.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() //area of circle
+{
+  float radius;             printf("\n");
+  printf("enter radius");
+  scanf("%f",&radius);
+  printf("area of circle is:%f",3.14*radius*radius);
+}
